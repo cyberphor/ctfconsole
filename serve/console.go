95f2e920436bcd94767dd664eeb4ef5348f45c8c
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-01-25 18:10:58 -0700

    Fixed a few more forms

--- serve/console.go
+++ serve/console.go
@@ -1,11 +1,22 @@
 package serve
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 
 	"github.com/cyberphor/ctfconsole/read"
 )
+
+func registerPage(w http.ResponseWriter, r *http.Request) {
+	page, err := template.ParseFiles("./templates/register.html")
+	if err != nil {
+		panic(err)
+	}
+	page.Execute(w, nil)
+	r.ParseForm()
+	fmt.Println(r)
+}
 
 func scoreboardPage(w http.ResponseWriter, r *http.Request) {
 	page, err := template.ParseFiles("./templates/scoreboard.html")
@@ -20,7 +31,11 @@ func Console() {
 	filePath := http.Dir("./templates/")
 	fileServer := http.FileServer(filePath)
 	http.Handle("/", fileServer)
+
 	scoreboardPageHandler := http.HandlerFunc(scoreboardPage)
 	http.Handle("/scoreboard.html", scoreboardPageHandler)
+
+	registerPageHandler := http.HandlerFunc(registerPage)
+	http.Handle("/register.html", registerPageHandler)
 	http.ListenAndServe(":8000", nil)
 }
