5dcc5ed2b89cce7e5417a702c5769b3651c56d86
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2023-11-22 04:40:45 +0000

    Debugging

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -14,10 +14,6 @@ import (
 )
 
 func main() {
-	// get handlers
-	health := health.Handler{}
-	player := player.Handler{}
-
 	// get api
 	app := fiber.New()
 	api := app.Group("/api")
@@ -27,23 +23,34 @@ func main() {
 	v1.Get("/ruok", health.Get)
 
 	// wire player handlers to player endpoint
-	v1.Post("/player", player.Create)
+	v1.Post("/player", player.Post)
+	v1.Get("/player", player.Get)
+	v1.Put("/player", player.Put)
+	v1.Delete("/player", player.Delete)
 
-	// wire admin handler to admin endpoint
+	// wire admin handlers to admin endpoint
+	v1.Post("/admin", admin.Post)
 	v1.Get("/admin", admin.Get)
-	v1.Post("/admin", admin.Update)
+	v1.Put("/admin", admin.Put)
+	v1.Delete("/admin", admin.Delete)
 
-	// wire team handler to team endpoint
+	// wire team handlers to team endpoint
+	v1.Post("/team", team.Post)
 	v1.Get("/team", team.Get)
-	v1.Post("/team", team.Update)
+	v1.Put("/team", team.Put)
+	v1.Delete("/team", team.Delete)
 
-	//  wire challenge handler to challenge endpoint
+	//  wire challenge handlers to challenge endpoint
+	v1.Post("/challenge", challenge.Post)
 	v1.Get("/challenge", challenge.Get)
-	v1.Post("/challenge", challenge.Update)
+	v1.Put("/challenge", challenge.Put)
+	v1.Delete("/challenge", challenge.Delete)
 
-	// wire scoreboard handler to scoreboard endpoint
+	// wire scoreboard handlers to scoreboard endpoint
+	v1.Post("/scoreboard", scoreboard.Post)
 	v1.Get("/scoreboard", scoreboard.Get)
-	v1.Post("/scoreboard", scoreboard.Update)
+	v1.Put("/scoreboard", scoreboard.Put)
+	v1.Delete("/scoreboard", scoreboard.Delete)
 
 	err := app.Listen(":8081")
 	if err != nil {
