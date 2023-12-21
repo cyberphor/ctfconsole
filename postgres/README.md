74f9a4c9c20518001cc6afc78ae71ceeb5617936
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2023-12-20 21:45:07 -0500

    Updating database schema

---
+++ postgres/README.md
@@ -0,0 +1,44 @@
+# Database Diagram
+
+```mermaid
+erDiagram
+    Team {
+        id INTEGER
+        name TEXT
+    }
+    Players {
+        id SERIAL
+        name TEXT
+        password TEXT
+        team INT
+    }
+    Campaigns {
+        id SERIAL
+        name TEXT
+    }
+    Challenges {
+        id SERIAL
+        name TEXT
+        points TEXT
+        campaign INT
+        team INT
+        solution TEXT
+    }
+    Scoreboards {
+        id SERIAL
+        name TEXT
+        team INT
+        score INT
+    }
+    Admins {
+        id SERIAL
+        name TEXT
+        password TEXT
+    }
+
+    Team ||--o{ Players : "team_id"
+    Campaigns ||--o{ Challenges : "campaign_id"
+    Team ||--o{ Challenges : "team_id"
+    Team ||--o{ Scoreboards : "team_id"
+    Team ||--o{ Admins : "team_id"
+```
---
+++ postgres/schema.sql
@@ -0,0 +1,44 @@
+CREATE TABLE IF NOT EXISTS teams (
+	id INTEGER PRIMARY KEY,
+	name      TEXT,
+	UNIQUE(name)
+);
+
+CREATE TABLE IF NOT EXISTS players (
+	id SERIAL PRIMARY KEY,
+	name  TEXT,
+	password  TEXT,
+	team INT REFERENCES teams(id)
+	UNIQUE(name)
+);
+
+CREATE TABLE IF NOT EXISTS campaigns (
+    id SERIAL PRIMARY KEY,
+	name      TEXT,
+	UNIQUE(name)
+);
+
+CREATE TABLE IF NOT EXISTS challenges (
+    id SERIAL PRIMARY KEY,
+	name      TEXT,
+	points    TEXT,
+	campaign  INT REFERENCES campaigns(id),
+	team      INT REFERENCES teams(id),
+	solution  TEXT,
+	UNIQUE(name)
+);
+
+CREATE TABLE IF NOT EXISTS scoreboards (
+	id SERIAL PRIMARY KEY,
+	name      TEXT,
+    team      INT REFERENCES teams(id),
+    score 	  INT,
+	UNIQUE(name)
+);
+
+CREATE TABLE IF NOT EXISTS admins (
+	id SERIAL PRIMARY KEY,
+	name  	  TEXT,
+	password  TEXT,
+	UNIQUE(name)
+);
